d8b2291704ef037569e2e00544f9320b44b3e518
shenyi <shenyi@round-table-union.com>
2020-07-29 09:21:47 +0800

    优化task1逻辑

--- week01/task1.py
+++ week01/task1.py
@@ -3,7 +3,19 @@ import pandas as pd
 from time import sleep
 from bs4 import BeautifulSoup as bs
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.26 Safari/537.36'
+USER_AGENT_LIST=[
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+import random
+user_agent = random.choice(USER_AGENT_LIST)
 
 header = {'user-agent': user_agent}
 
@@ -15,23 +27,22 @@ response = requests.get(myurl, headers=header)
 
 bs_info = bs(response.text, 'html.parser')
 
-
 def getMovieDetail(url):
     detailUrl = host + url
+    user_agent = random.choice(USER_AGENT_LIST)
     detail_header = {'user-agent': user_agent, 'referer': myurl}
     response = requests.get(detailUrl, headers=detail_header)
     movie_detail = bs(response.text, 'html.parser')
     divtag = movie_detail.find_all(
         'div', attrs={'class': 'movie-brief-container'})[0]
     movie_name = divtag.find_all('h1', attrs={'class': 'name'})[0].text
-    lutag = divtag.find_all('ul')[0]
+    ultag = divtag.find_all('ul')[0]
     category = ''
-    for atag in lutag.find_all('li')[0].find_all('a'):
+    for atag in ultag.find_all('li')[0].find_all('a'):
         s = atag.text.replace(' ', '')
         category = category + s + ' '
-    showtime = lutag.find_all('li')[2]
+    showtime = ultag.find_all('li')[2]
     return {'name': movie_name, 'category': category, 'showtime': showtime.text}
-
 
 movie_count = 10
 
@@ -42,7 +53,7 @@ for itag in bs_info.find_all('div', attrs={'class': 'movie-item-hover'}):
         if movie_count < 1:
             break
         movie_detail = getMovieDetail(atag.get('href'))
-        sleep(10)
+        sleep(3)
         movie_list.append(movie_detail)
         movie_count -= 1
 
